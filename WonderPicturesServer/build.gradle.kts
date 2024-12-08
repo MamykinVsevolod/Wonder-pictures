c5d2d4f6714f2b466c2a9f8d359b23d83bc24e62
MamykinVsevolod <vsevolodmamykin@gmail.com>
2024-12-08 04:09:30 +0300

    configured the server

--- WonderPicturesServer/build.gradle.kts
+++ WonderPicturesServer/build.gradle.kts
@@ -1,5 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.application) apply false // Android клиент
+    alias(libs.plugins.kotlin.android) apply false // Kotlin для Android
+    id("org.springframework.boot") version "3.1.4" apply false // Spring Boot
+    id("io.spring.dependency-management") version "1.1.3" apply false // Spring Dependency Management
+    kotlin("jvm") version "1.9.0" apply false // Kotlin для Spring Boot
 }
